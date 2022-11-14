094e623555d0e564e8baad995a6a6c0c1558f131
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-14 08:20:32 -0700

    finishes tests

--- test/HelloWorld.js
+++ test/HelloWorld.js
@@ -30,19 +30,31 @@ describe("HelloWorld", async () => {
 	describe("set message", async () => {
 		describe("success", async () => {
 			it("changes the message", async () => {
+				const {helloWorld} = await loadFixture(deployHelloWorldFixture);
 
+				await helloWorld.setMessage("Setting new message");
+				const expectedMessage = "Setting new message";
+				const currMessage = await helloWorld.getMessage();
+
+				assert(expectedMessage === currMessage, "Messages did not match");
 			});
 			it("emits the MessageChanged event", async () => {
+				const {helloWorld} = await loadFixture(deployHelloWorldFixture);
 
+				await expect(helloWorld.setMessage("Setting new message")).to.emit(helloWorld, "MessageChanged").withArgs("Setting new message");
 			});
 		});
 
 		describe("failure", async () => {
 			it("reverts when non sender tries to change message", async () => {
+				const {helloWorld, otherAccount} = await loadFixture(deployHelloWorldFixture);
 
+				await expect(helloWorld.connect(otherAccount).setMessage("Setting new message")).to.be.revertedWith("Caller must be the owner");
 			});
 			it("reverts when sender sends an empty string", async () => {
+				const {helloWorld} = await loadFixture(deployHelloWorldFixture);
 
+				await expect(helloWorld.setMessage("")).to.be.revertedWith("Empty string not allowed");
 			});
 		});
 	});
