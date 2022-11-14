4a8af3c5cc342f8abbbfb20793bb85f97a33c305
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-14 08:24:03 -0700

    deploy script modified

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,26 +6,14 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const main = async () => {
+	const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
+	const helloWorld = await HelloWorld.deploy("Initial message here");
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+	console.log(`HelloWorld contract deployed to ${helloWorld.address}`);
+};
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
