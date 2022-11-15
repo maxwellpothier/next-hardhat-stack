736bf2ea3a73deb0ce7906e94520c7f99df27f90
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-14 20:14:34 -0700

    frontend change message

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,24 @@ import HelloWorld from "../artifacts/contracts/HelloWorld.sol/HelloWorld.json";
 import useIsMounted from "./hooks/useIsMounted";
 
 const Home = (props) => {
-	console.log(props);
+	const contractMessage = props.currMessage;
 
 	const {address} = useAccount();
 	const mounted = useIsMounted();
+
+	const changeMessage = async () => {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		const contract = new ethers.Contract(contractAddress, HelloWorld.abi, signer);
+
+		try {
+			console.log(address);
+			await contract.setMessage("This is the new message");
+		} catch (err) {
+			console.log("ERROR OCCURRED: ", err);
+		}
+
+	};
 
 	return (
 		<div>
@@ -19,6 +33,8 @@ const Home = (props) => {
 					<p>My address is {address}</p> :
 					null
 			}
+			{contractMessage}
+			<button onClick={changeMessage}>change message</button>
 		</div>
 	);
 };
