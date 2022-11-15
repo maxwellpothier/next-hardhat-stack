3cd4d94c143d1784c638696f51ded652160f474e
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-14 19:18:22 -0700

    adds rainbowkit connect button to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import {infuraProvider} from "wagmi/providers/infura";
+import {publicProvider} from "wagmi/providers/public";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const {chains, provider} = configureChains(
+	[chain.goerli],
+	[
+		infuraProvider({
+			apiKey: process.env.RPC_URL,
+		}),
+		publicProvider(),
+	],
+);
 
-export default MyApp
+const {connectors} = getDefaultWallets({
+	appName: "Hello World Messaging",
+	chains,
+});
+
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+})
+
+const App = ({Component, pageProps}) => {
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider chains={chains}>
+				<Component {...pageProps}/>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
+};
+
+export default App;
