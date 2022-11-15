b0bd139815665e4a08a7b92775b3f42edfc2b222
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-14 19:24:02 -0700

    fixes hydration issue

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount } from "wagmi";
+import useIsMounted from "./hooks/useIsMounted";
 
 const Home = () => {
+	const {address} = useAccount();
+	const mounted = useIsMounted();
+
 	return (
 		<div>
 			<ConnectButton/>
+			{mounted ?
+				address &&
+					<p>My address is {address}</p> :
+					null
+			}
 		</div>
 	);
 };
